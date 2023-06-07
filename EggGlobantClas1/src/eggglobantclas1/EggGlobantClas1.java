aee3077d1884b313daa57982c91b843b1f52afd0
Alfredo Delnardo <alfredodelnardo@gmail.com>
2023-06-07 19:00:07 -0300

    Primera clase de Java + Ejercicios

---
+++ EggGlobantClas1/src/eggglobantclas1/EggGlobantClas1.java
@@ -0,0 +1,27 @@
+
+package eggglobantclas1;
+
+/**
+ *
+ * @author Alfredo Delnardo
+ */
+public class EggGlobantClas1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Primera clase de java");
+        
+int numero = 48;
+double decimales = 3.55;
+boolean bandera = false;
+/*
+Para imprimir, y "\n" para salto de linea
+*/
+System.out.println("El numero es: "+numero+", El decimal es: "+decimales+", el boolean es: "+bandera+"\n"+"Decimal: "+decimales);
+
+                
+    }
+    
+}
---
+++ EggGlobantClas1/src/eggglobantclas1/Ejer01.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eggglobantclas1;
+
+/**
+ *
+ * @author Alfredo Delnardo
+ */
+public class Ejer01 {
+
+    public static void main(String[] args) {
+
+        int num1 = 5;
+        int num2 = 10;
+        int suma = num1 + num2;
+        double division = num2 / num1;
+        boolean bandera = num2 > num1;
+        System.out.println("Num1=" + num1 + "\n" + "Num2=" + num2 + "\n" + "Suma=" + suma + "\n" + "Division=" + division);
+        System.out.println("Boolean="+bandera);
+    }
+
+}
+
